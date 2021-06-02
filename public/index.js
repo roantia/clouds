1a54f50ce19aed81111231eb1f35581302e77075
Pınar Kazak <pinarkazak@Pnar-MacBook-Pro-6.local>
2021-06-02 14:03:29 +0200

    website css changed

--- public/index.js
+++ public/index.js
@@ -1,29 +1,16 @@
+const popupOverlay = document.querySelector('.popup-overlay')
+let visible
 
-
-$(function() {
-    // Open Popup
-    $('[popup-open]').on('click', function() {
-        var popup_name = $(this).attr('popup-open');
- $('[popup-name="' + popup_name + '"]').fadeIn(300);
-    });
-
-    // Close Popup
-    $('[popup-close]').on('click', function() {
- var popup_name = $(this).attr('popup-close');
- $('[popup-name="' + popup_name + '"]').fadeOut(300);
-    });
-
-    // Close Popup When Click Outside
-    $('.popup').on('click', function() {
- var popup_name = $(this).find('[popup-close]').attr('popup-close');
- $('[popup-name="' + popup_name + '"]').fadeOut(300);
-    }).children().click(function() {
- return false;
-    });
-
-});
-
-$('a.yourlink').click(function(e) {
-    e.preventDefault();
-    window.open('https://www.notion.so/GEO-CODE-MODE-f715bd087dc443259f3028d00de57fc8');
-  });
+function togglePopup(element) {
+    visible = element.id
+    const popup = document.querySelector('#' + visible+ '.popup')
+    
+    popup.style.display = 'block'
+    popupOverlay.style.display = 'block'
+}
+function closePopup(element) {
+    const popup = document.querySelector('#' + visible+ '.popup')
+    
+    popup.style.display = 'none'
+    popupOverlay.style.display = 'none'
+}
